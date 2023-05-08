89be593dc2cec87442297ea3a22cc4ebf8529c75
幻弦让叶 <1248389474@qq.com>
2023-05-08 11:20:49 +0800

    新增选择图片

--- app/src/main/java/com/lanier/memories/InsertItemAct.kt
+++ app/src/main/java/com/lanier/memories/InsertItemAct.kt
@@ -2,10 +2,29 @@ package com.lanier.memories
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.material.imageview.ShapeableImageView
 
 class InsertItemAct : AppCompatActivity() {
+
+    private val ivPic by lazy {
+        findViewById<ShapeableImageView>(R.id.ivPic)
+    }
+
+    private val selectPicResult = registerForActivityResult(
+        ActivityResultContracts.GetContent()
+    ) {
+        it?.let { uri ->
+            ivPic.setImageURI(uri)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_insert_item)
+
+        ivPic.setOnClickListener {
+            selectPicResult.launch("image/*")
+        }
     }
 }
