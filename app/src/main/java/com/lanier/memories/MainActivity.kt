4bb1ffb4281b68bbc97e938a33060d6a5fffb96d
Turtledove <339962954@qq.com>
2023-05-08 17:26:55 +0800

    调整

--- app/src/main/java/com/lanier/memories/MainActivity.kt
+++ app/src/main/java/com/lanier/memories/MainActivity.kt
@@ -1,22 +1,35 @@
 package com.lanier.memories
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.imageview.ShapeableImageView
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+
+    private val rv by lazy {
+        findViewById<RecyclerView>(R.id.recyclerView)
+    }
+    private val mAdapter by lazy {
+        MA()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rv = findViewById<RecyclerView>(R.id.recyclerView)
         rv.apply {
-            adapter = MA()
+            adapter = mAdapter
             layoutManager = GridLayoutManager(this@MainActivity, 2)
         }
 
@@ -25,9 +38,31 @@ class MainActivity : AppCompatActivity() {
                 start<InsertItemAct> {  }
             }
     }
+
+    override fun onStart() {
+        super.onStart()
+        lifecycleScope
+            .launch {
+                val list = withContext(Dispatchers.IO) {
+                    MemoriesRoomHelper.getAllMemories()
+                }
+                mAdapter
+                    .data = list
+            }
+    }
 }
 
 class MA: RecyclerView.Adapter<VH>() {
+
+    private val _data = mutableListOf<MemoriesData>()
+    var data: List<MemoriesData>
+        get() = _data
+        set(value) {
+            _data.clear()
+            _data.addAll(value)
+            notifyDataSetChanged()
+        }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
         return VH(
             LayoutInflater.from(parent.context)
@@ -35,9 +70,15 @@ class MA: RecyclerView.Adapter<VH>() {
         )
     }
 
-    override fun getItemCount() = 9
+    override fun getItemCount() = _data.size
 
-    override fun onBindViewHolder(holder: VH, position: Int) {}
+    override fun onBindViewHolder(holder: VH, position: Int) {
+        val uri = Uri.parse(_data[position].path)
+        holder.iv.setImageURI(uri)
+    }
 }
 
-class VH(view: View): RecyclerView.ViewHolder(view)
+class VH(view: View): RecyclerView.ViewHolder(view) {
+
+    val iv = view.findViewById<ShapeableImageView>(R.id.ivPic)
+}
