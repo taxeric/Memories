89d5cb5e4f95f2891b88fe54724aadf5eef968aa
Turtledove <339962954@qq.com>
2023-05-11 11:56:21 +0800

    UI调整

--- app/src/main/java/com/lanier/memories/MemoriesGlanceWidget.kt
+++ app/src/main/java/com/lanier/memories/MemoriesGlanceWidget.kt
@@ -7,27 +7,38 @@ import android.os.Build
 import android.provider.MediaStore
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.glance.BitmapImageProvider
+import androidx.glance.Button
 import androidx.glance.GlanceModifier
 import androidx.glance.Image
 import androidx.glance.LocalContext
+import androidx.glance.action.ActionParameters
+import androidx.glance.action.actionParametersOf
+import androidx.glance.action.actionStartActivity
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.background
 import androidx.glance.color.dynamicThemeColorProviders
 import androidx.glance.currentState
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Box
 import androidx.glance.layout.Column
 import androidx.glance.layout.Row
+import androidx.glance.layout.Spacer
 import androidx.glance.layout.fillMaxHeight
 import androidx.glance.layout.fillMaxSize
 import androidx.glance.layout.fillMaxWidth
+import androidx.glance.layout.height
 import androidx.glance.layout.padding
+import androidx.glance.layout.size
 import androidx.glance.layout.width
 import androidx.glance.state.GlanceStateDefinition
 import androidx.glance.text.FontWeight
 import androidx.glance.text.Text
 import androidx.glance.text.TextAlign
+import androidx.glance.text.TextDecoration
 import androidx.glance.text.TextStyle
 
 /**
@@ -51,20 +62,19 @@ class MemoriesGlanceWidget: GlanceAppWidget() {
             if (memoriesDataList.isNotEmpty()) {
                 val memoriesData = memoriesDataList[0]
                 val bitmap = obtainBitmap(Uri.parse(memoriesData.path))
-                Image(
-                    provider = BitmapImageProvider(bitmap = bitmap),
-                    contentDescription = "",
-                    modifier = GlanceModifier
-                        .fillMaxHeight()
-                        .width(50.dp)
-                )
                 Column(
                     modifier = GlanceModifier
-                        .fillMaxSize()
+                        .fillMaxHeight()
+                        .width(250.dp)
+                        .padding(
+                            vertical = 12.dp,
+                            horizontal = 16.dp
+                        )
                 ) {
                     Text(
                         text = memoriesData.name,
                         style = TextStyle(
+                            fontSize = 18.sp,
                             fontWeight = FontWeight.Bold,
                             textAlign = TextAlign.Center,
                         ),
@@ -76,14 +86,65 @@ class MemoriesGlanceWidget: GlanceAppWidget() {
                         text = memoriesData.desc,
                         style = TextStyle(
                             fontSize = 12.sp,
+                            textDecoration = TextDecoration.Underline
                         ),
                         modifier = GlanceModifier
                             .fillMaxSize()
                             .padding(8.dp)
                     )
                 }
+                Column(
+                    modifier = GlanceModifier
+                        .fillMaxHeight()
+                        .width(1.dp)
+                        .background(Color.LightGray)
+                ) {}
+                Column(
+                    modifier = GlanceModifier
+                        .fillMaxSize(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Image(
+                        provider = BitmapImageProvider(bitmap = bitmap),
+                        contentDescription = "",
+                        modifier = GlanceModifier
+                            .size(50.dp)
+                    )
+                    Spacer(
+                        modifier = GlanceModifier
+                            .fillMaxWidth()
+                            .height(24.dp)
+                    )
+                    Button(
+                        text = "→",
+                        style = TextStyle(
+                            textAlign = TextAlign.Center
+                        ),
+                        onClick = actionStartActivity(
+                            MemoriesDetailsAct::class.java,
+                            actionParametersOf(
+                                ActionParameters.Key<Int>("M_ID") to memoriesData.id
+                            )
+                        ),
+                        modifier = GlanceModifier
+                            .width(80.dp)
+                    )
+                }
             } else {
-                Text(text = "点击选择")
+                Box(
+                    modifier = GlanceModifier
+                        .fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Button(
+                        text = "点击选择",
+                        onClick = actionStartActivity(
+                            MainActivity::class.java
+                        ),
+                        modifier = GlanceModifier
+                    )
+                }
             }
         }
     }
